6e97f45901be0d57f221cf3022b3d7cf805608f0
YanisXTodoroki <fereceanyanis@gmail.com>
2021-06-18 00:11:35 +0200

    changes
    changed ship spwantime after the start scene

--- SchiffeSpiel/Assets/Scripts/ObstacleSpawn.cs
+++ SchiffeSpiel/Assets/Scripts/ObstacleSpawn.cs
@@ -4,42 +4,44 @@ using UnityEngine;
 
 public class ObstacleSpawn : MonoBehaviour
 {
-	public GameObject ship;
-	public GameObject stone;
-	private GameObject randomObstacle;
-	public float minX;
-	public float maxX;
-	public float minY;
-	public float maxY;
-	public float timeToSpawn;
-	private float spawnTime;
-	
+    public GameObject ship;
+    public GameObject stone;
+    private GameObject randomObstacle;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    public float timeToSpawn;
+    private float spawnTime;
+
     // Update is called once per frame
     void Update()
     {
-		int random = Random.Range(0,2);
-		if(random == 1)
-        {
-			randomObstacle = ship;
-		}
-		else if(random == 0)
-        {
-			randomObstacle = stone;
-		}
 
-		if (Time.time > spawnTime)
-		{
-			Spawn();
-			spawnTime = Time.time + timeToSpawn;
-		}
-		
+        int random = Random.Range(0, 2);
+        if (random == 1)
+        {
+            randomObstacle = ship;
+        }
+        else if (random == 0)
+        {
+            randomObstacle = stone;
+        }
+
+        if (Time.time > spawnTime)
+        {
+            Spawn();
+            spawnTime = Time.time + timeToSpawn;
+        }
     }
-	void Spawn()
-	{
-		float randomX = Random.Range(minX, maxX);
-		float randomY = Random.Range(minY, maxY);
-		
-		Instantiate(randomObstacle, transform.position + new Vector3(randomX, randomY,0), transform.rotation);
-	}
-	
+    void Spawn()
+    {
+        if (Time.timeSinceLevelLoad> 5)
+        {
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+
+            Instantiate(randomObstacle, transform.position + new Vector3(randomX, randomY, 0), transform.rotation);
+        }
+    }
 }
