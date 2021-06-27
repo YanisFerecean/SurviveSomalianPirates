60933dd81d0bbc8243654bfbc83fca61c7c31e8f
coderalberto <48130185+AlbertoSacilotto@users.noreply.github.com>
2021-06-27 21:55:00 +0200

    Added features
    Added collison of player with obstacles & game over interface with restart button. To be fixed: Game Over Interface doesn't follow camera (Needed fix!!)

---
+++ SchiffeSpiel/Assets/Scripts/GameOver.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    void Update()
+    {
+        if(GameObject.FindGameObjectWithTag("Player") == null)
+        {
+            gameOverPanel.SetActive(true);
+        }  
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
